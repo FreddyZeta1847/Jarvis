fa461210c93f285f359e784d35bdf8953ba24e20
Federico Santini <santinifederico06@gmail.com>
2026-02-15 18:15:38 +0100

    Install deps on Azure machine at startup

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
-if [ -d "antenv" ]; then
-    source antenv/bin/activate
-fi
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+python3 -m pip install -r requirements.txt --quiet
+python3 -m gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
