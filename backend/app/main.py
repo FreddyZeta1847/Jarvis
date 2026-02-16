485fc2ead72d7c38b2865bba64a99a1407a51fde
Federico Santini <santinifederico06@gmail.com>
2026-02-16 01:47:40 +0100

    Connect frontend to backend: CORS + API URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://localhost:3000",
+        "https://delightful-sand-09063ba03.4.azurestaticapps.net",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
